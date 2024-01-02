15a66139260db1c01d185a81a86a961722ccc409
painiago <95057970+painiago@users.noreply.github.com>
2024-01-02 14:20:51 -0300

    atualizando analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,9 +3,34 @@ import '@/styles/globals.scss'
 import type { AppProps } from 'next/app'
 // import { SessionProvider as NextAuthProvider } from 'next-auth/react';
 import  { Footer } from '@/pages/footer';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
-
+declare global {
+  interface Window {
+    // Adicione a declaração para dataLayer aqui
+    dataLayer: any[];
+    gtag?: (...args: any[]) => void;
+  }
+}
 export default function App({ Component, pageProps }: AppProps) {
+
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      if (window.gtag) {
+        window.gtag('config', process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS, {
+          page_path: url,
+        });
+      }
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return (
     
     <>
